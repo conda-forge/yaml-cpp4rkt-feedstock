cc49ba13d7371eff4c42b4b55e7c5a259367d0e5
Allan Leal <allanmulin@gmail.com>
2021-07-06 13:58:43 +0200

    Further fixes

--- recipe/test/app/main.cpp
+++ recipe/test/app/main.cpp
@@ -1,12 +1,9 @@
-#include <cassert>
 #include <iostream>
 #include <yaml-cpp/yaml.h>
 
 int main()
 {
     YAML::Node node = YAML::Load("[1, 2, 3]");
-
-    assert(node.IsSequence());
 
     std::cout << "Successful execution of application that links against yaml-cpp4rkt!" << std::endl;
 
