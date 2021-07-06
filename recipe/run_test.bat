e49a9583edad9d23c0b5d9f222d2d0f56f82c59b
Allan Leal <allanmulin@gmail.com>
2021-07-06 14:51:43 +0200

    Correct @REM comment

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -1,8 +1,12 @@
 cd test\app
 mkdir build
 cd build
+
+@REM WARNING: If testing yaml-cpp app is not built in Release, its execution
+@REM returns a non zero %errorlevel% and the conda build task fails!
+
 cmake -GNinja ..                         ^
-    -DCMAKE_BUILD_TYPE=Release           ^  @REM WARNING: If app dependent on yaml-cpp is not build in Release, its execution returns a non zero %errorlevel% and conda build task fails!
+    -DCMAKE_BUILD_TYPE=Release           ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX%
 ninja
 app.exe
