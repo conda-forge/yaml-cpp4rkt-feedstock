64a7425294cf0d97fa4cad08b1c4ccece856c62b
Allan Leal <allanmulin@gmail.com>
2021-07-06 14:21:05 +0200

    Fix test by enforcing Release build for it as well

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -1,6 +1,8 @@
 cd test\app
 mkdir build
 cd build
-cmake -GNinja .. -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX%
+cmake -GNinja ..                         ^
+    -DCMAKE_BUILD_TYPE=Release           ^  @REM WARNING: If app dependent on yaml-cpp is not build in Release, its execution returns a non zero %errorlevel% and conda build task fails!
+    -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX%
 ninja
 app.exe
